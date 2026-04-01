3fae4dd7554434ebec9172ce2a7cbb596042b5d1
LukyanenkoDmitriy <124089859+LukyanenkoDmitriy@users.noreply.github.com>
2026-04-01 17:30:03 +0300

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+   topKFrequent(nums, k) {
+	let result = {}
+
+	nums.forEach(num => {
+		if (result[num]) result[num]++
+		else result[num] = 1
+	})
+	result = Object.keys(result).sort((a, b) => result[b] - result[a])
+
+	return result.splice(0, k)
+}
+}
